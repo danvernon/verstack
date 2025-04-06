44be77fefc8ded0a83d93ad14eb98d9d9c6b2b9d
Daniel Vernon <danpvernon@gmail.com>
2025-04-06 22:14:00 +0100

    trpc fixes

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,65 +1,31 @@
 "use client";
 
-import { QueryClientProvider, useQuery } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Link, Route, Routes } from "react-router";
-import { getQueryClient } from "./providers/trpc";
-import { useState } from "react";
-import { AppRouter } from "@/server/api/root";
-import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 
-import { TRPCProvider, useTRPC } from "@/utils/trpc";
-import { env } from "@/env";
-import SuperJSON from "superjson";
+import { trpc, TRPCProvider } from "@/utils/trpc";
 
 export default function App() {
-  const queryClient = getQueryClient();
-  const [trpcClient] = useState(() =>
-    createTRPCClient<AppRouter>({
-      links: [
-        loggerLink({
-          enabled: (op) =>
-            env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
-        }),
-        httpBatchLink({
-          transformer: SuperJSON,
-          url: getBaseUrl() + "/api/trpc",
-          headers: () => {
-            const headers = new Headers();
-            headers.set("x-trpc-source", "nextjs-react");
-            return headers;
-          },
-        }),
-      ],
-    }),
-  );
-
   return (
     <ErrorBoundary
       fallbackRender={(props) => <div>Error: {props.error.message}</div>}
     >
-      <QueryClientProvider client={queryClient}>
-        <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
-          {/* // SkewProtectionBuster // PostHog */}
-          <BrowserRouter>
-            {/* Force Onboarding */}
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </BrowserRouter>
-        </TRPCProvider>
-      </QueryClientProvider>
+      <TRPCProvider>
+        {/* // SkewProtectionBuster // PostHog */}
+        <BrowserRouter>
+          {/* Force Onboarding */}
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </TRPCProvider>
     </ErrorBoundary>
   );
 }
 
 function Dashboard() {
-  const trpc = useTRPC();
-  const { data, error, isLoading } = useQuery(
-    trpc.posts.getLatest.queryOptions(),
-  );
+  const { data, isLoading, error } = trpc.posts.getLatest.useQuery();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -86,10 +52,4 @@ function Home() {
       {/* will either be home.tsx or settings.tsx */}
     </div>
   );
-}
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
 }
--- src/utils/trpc.ts
+++
@@ -1,5 +0,0 @@
-import type { AppRouter } from "@/server/api/root";
-import { createTRPCContext } from "@trpc/tanstack-react-query";
-
-export const { TRPCProvider, useTRPC, useTRPCClient } =
-  createTRPCContext<AppRouter>();
